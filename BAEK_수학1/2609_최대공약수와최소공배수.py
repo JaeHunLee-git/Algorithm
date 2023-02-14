765022be519e906b5025a7fa78cc46ec9bc7f1d0
Lee Jae-Hun <joz526@naver.com>
2023-02-14 22:35:38 +0900

    py

---
+++ BAEK_수학1/2609_최대공약수와최소공배수.py
@@ -0,0 +1,10 @@
+a, b = map(int, input().split())
+tmp = min(a, b)
+min = 0
+max = 0
+for i in range(1, tmp + 1):
+    if a % i == 0 and b % i == 0 and i > max:
+        max = i
+        min = max * (a // i) * (b // i)
+print(max)
+print(min)
