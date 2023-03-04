ab7eeb26435c87d1608f0458edaa72afa3e82830
Lee Jae-Hun <joz526@naver.com>
2023-03-04 13:49:14 +0900

    py

---
+++ BAEK_브루트포스/10819_차이를최대로.py
@@ -0,0 +1,17 @@
+# 패턴은 없고 범위가 작으므로 브루트포스
+
+import sys
+from itertools import permutations
+
+input = sys.stdin.readline
+n = int(input())
+a = sorted(list(map(int, input().split())))
+lst = list(permutations(a, n))
+sum = []
+for i in lst:
+    tmp = 0
+    for k in range(len(i) - 1):
+        tmp += abs(i[k] - i[k + 1])
+    sum.append(tmp)
+print(max(sum))
+
