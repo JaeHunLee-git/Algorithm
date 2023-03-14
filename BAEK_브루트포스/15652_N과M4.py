ba8884049ddf02a31a390f026d6906a4b2510ab3
Lee Jae-Hun <joz526@naver.com>
2023-03-14 15:29:14 +0900

    py

---
+++ BAEK_브루트포스/15652_N과M4.py
@@ -0,0 +1,19 @@
+import sys
+
+input = sys.stdin.readline
+
+
+def dfs(x):
+    if len(s) == m:
+        print(" ".join(map(str, s)))
+        return
+    for i in range(x, n + 1):
+        s.append(i)
+        dfs(i)
+        s.pop()
+
+
+n, m = map(int, input().split())
+s = []
+
+dfs(1)
