e278ee1b7d43b7881e5355e3d8d08acab2f00874
jaehunLee-git <joz526@naver.com>
2023-09-11 09:11:14 +0900

    py

---
+++ BAEK_분할정복/10816_숫자카드2.py
@@ -0,0 +1,19 @@
+import sys
+
+input = sys.stdin.readline
+
+n = int(input().strip())
+arr1 = list(map(int, input().split()))
+m = int(input().strip())
+arr2 = list(map(int, input().split()))
+
+visit = [0] * 20000001
+
+for i in arr1:
+    visit[i - 10000001] += 1
+
+for i in arr2:
+    if visit[i - 10000001] != 0:
+        print(visit[i - 10000001], end=" ")
+    else:
+        print(0, end=" ")
