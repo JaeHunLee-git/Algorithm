f4980aacc38acb87b9e945d448fe6924c410538b
jaehunLee-git <joz526@naver.com>
2023-09-12 12:44:55 +0900

    py

---
+++ BAEK_분할정복/1891_사분면.py
@@ -0,0 +1,45 @@
+# 어려움
+d, num = input().split()
+d = int(d)
+x, y = map(int, input().split())
+
+
+def find_coordinate(num, idx, r, c, size):
+    if size == 0:
+        global num_r, num_c
+        num_r, num_c = r, c
+        return
+
+    if num[idx] == "1":
+        find_coordinate(num, idx + 1, r, c + size, size // 2)
+    elif num[idx] == "2":
+        find_coordinate(num, idx + 1, r, c, size // 2)
+    elif num[idx] == "3":
+        find_coordinate(num, idx + 1, r + size, c, size // 2)
+    elif num[idx] == "4":
+        find_coordinate(num, idx + 1, r + size, c + size, size // 2)
+
+
+def sol(num_r, num_c, size, ans):
+    if size == 0:
+        print(ans)
+        return
+
+    if 0 <= num_r < size and size <= num_c < 2 * size:
+        sol(num_r, num_c - size, size // 2, ans + "1")
+    elif 0 <= num_r < size and 0 <= num_c < size:
+        sol(num_r, num_c, size // 2, ans + "2")
+    elif size <= num_r < 2 * size and 0 <= num_c < size:
+        sol(num_r - size, num_c, size // 2, ans + "3")
+    elif size <= num_r < 2 * size and size <= num_c < 2 * size:
+        sol(num_r - size, num_c - size, size // 2, ans + "4")
+
+
+num_r, num_c = 0, 0
+find_coordinate(num, 0, num_r, num_c, (2**d) // 2)  # 좌표 찾기
+num_r -= y
+num_c += x
+if 0 <= num_r < 2**d and 0 <= num_c < 2**d:  # 사분면 안 쪽 범위면
+    sol(num_r, num_c, (2**d) // 2, "")
+else:  # 범위를 벗어날 시
+    print(-1)
