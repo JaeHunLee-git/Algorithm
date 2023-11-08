207d2fa5ba23d75ff3178c5f7686458eb56a13d0
jaehunLee-git <joz526@naver.com>
2023-11-08 17:40:52 +0900

    py

--- BAEK_자료구조2/1655_가운데를말해요.py
+++ BAEK_자료구조2/1655_가운데를말해요.py
@@ -9,9 +9,9 @@ right_heap = []
 for i in range(n):
     tmp = int(input())
     if len(left_heap) == len(right_heap):
-        hq.heappush(left_heap, (-tmp, tmp))
+        hq.heappush(left_heap, (-tmp, tmp))  ## 최대힙 : 최대값 제일 앞에
     else:
-        hq.heappush(right_heap, (tmp, tmp))
+        hq.heappush(right_heap, (tmp, tmp))  ## 최소힙 : 최소값 제일 앞에
     if right_heap and left_heap[0][1] > right_heap[0][0]:
         min = hq.heappop(right_heap)[0]
         max = hq.heappop(left_heap)[1]
