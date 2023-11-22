677fc9af87572ded8e30474ba8442ca0817a8a4d
jaehunLee-git <joz526@naver.com>
2023-11-22 14:21:49 +0900

    py

---
+++ BAEK_DP2/15989_1,2,3더하기4.py
@@ -0,0 +1,16 @@
+import sys
+
+input = sys.stdin.readline
+t = int(input())
+
+for _ in range(t):
+    n = int(input())
+    dp = [1] * (n + 1)
+
+    for i in range(2, n + 1):
+        dp[i] += dp[i - 2]
+
+    for i in range(3, n + 1):
+        dp[i] += dp[i - 3]
+
+    print(dp[n])
