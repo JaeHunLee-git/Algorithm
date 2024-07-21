8e3ab138ad07fd6e4678bd9e2a1b33891bd452a5
jaehunLee-git <joz526@naver.com>
2024-07-21 20:41:37 +0900

    BFS S1

--- STUDY_DFSBFS/1389_케빈베이컨의6단계법칙.py
+++ STUDY_DFSBFS/1389_케빈베이컨의6단계법칙.py
@@ -1,12 +1,35 @@
 import sys
+from collections import deque
 
 input = sys.stdin.readline
 
+
+def bfs(v):
+    q = deque([v])
+    visited[v] = 1
+    while q:
+        target = q.popleft()
+
+        for i in graph[target]:
+            if not visited[i]:
+                visited[i] = visited[target] + 1
+                q.append(i)
+
+
 n, m = map(int, input().split())
-user = [[] for _ in range(n + 1)]
-relation = [[] for _ in range(n + 1)]
+
+graph = [[] for _ in range(n + 1)]
 
 for _ in range(m):
     a, b = map(int, input().split())
-    relation[a].append(b)
-    relation[b].append(a)
+    graph[a].append(b)
+    graph[b].append(a)
+
+result = []
+
+for i in range(1, n + 1):
+    visited = [0] * (n + 1)
+    bfs(i)
+    result.append(sum(visited))
+
+print(result.index(min(result)) + 1)
