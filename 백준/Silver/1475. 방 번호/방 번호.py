f174865efbe560562ec930bcd48e24fa12f819bb
LEE JAE HUN <118654191+JaeHunLee-git@users.noreply.github.com>
2024-12-28 19:49:06 +0900

    [Silver V] Title: 방 번호, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/1475. 방 번호/방 번호.py
@@ -0,0 +1,20 @@
+import sys
+
+input = sys.stdin.readline
+n = int(input())
+arr = [0] * 10
+
+while n > 0:
+    tmp = n % 10
+    arr[tmp] += 1
+    n //= 10
+
+tmp = arr[6] + arr[9]
+if tmp % 2 == 0:
+    ans = (arr[9] + arr[6]) // 2
+else:
+    ans = ((arr[9] + arr[6]) // 2) + 1
+arr[9], arr[6] = 0, 0
+if max(arr) > ans:
+    ans = max(arr)
+print(ans)
