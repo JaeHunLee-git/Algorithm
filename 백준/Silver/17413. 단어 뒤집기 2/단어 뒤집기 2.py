f062f53cec3a9bba3989429ae8240b19f941d3fa
LEE JAE HUN <118654191+JaeHunLee-git@users.noreply.github.com>
2025-01-06 00:09:55 +0900

    [Silver III] Title: 단어 뒤집기 2, Time: 484 ms, Memory: 31544 KB -BaekjoonHub

---
+++ 백준/Silver/17413. 단어 뒤집기 2/단어 뒤집기 2.py
@@ -0,0 +1,29 @@
+import sys
+
+text = list(sys.stdin.readline().rstrip())
+
+
+flag = False  # 정상 출력은 True
+result = ""
+stack = ""
+
+for i in text:
+    if flag == False:
+        if i == "<":
+            flag = True
+            stack += i
+        elif i == " ":
+            stack += i
+            result += stack
+            stack = ""
+        else:
+            stack = i + stack
+
+    elif flag == True:
+        stack += i
+        if i == ">":
+            flag = False
+            result += stack
+            stack = ""
+
+print(result + stack)
